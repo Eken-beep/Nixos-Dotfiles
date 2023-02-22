852ae0faed432d8727fdf8d61627f561429794e9
Edvin Källström <edvin.kallstrom@protonmail.com>
2023-02-22 06:57:49 +0100

    fix

--- home/other/init.lua
+++ home/other/init.lua
@@ -32,8 +32,63 @@ g.mapleader = ' '
 g.maplocalleader = ' '
 
 -- Theme
-require('lualine').setup()
-local ok, _ = pcall(vim.cmd, 'auto')
+local colors = {
+  blue   = '#89b4fa',
+  cyan   = '#89dceb',
+  black  = '#1e1e2e',
+  white  = '#cdd6f4',
+  red    = '#f38ba8',
+  violet = '#cba6f7',
+  grey   = '#181825',
+}
+
+local bubbles_theme = {
+  normal = {
+    a = { fg = colors.black, bg = colors.violet },
+    b = { fg = colors.white, bg = colors.grey },
+    c = { fg = colors.black, bg = colors.black },
+  },
+
+  insert = { a = { fg = colors.black, bg = colors.blue } },
+  visual = { a = { fg = colors.black, bg = colors.cyan } },
+  replace = { a = { fg = colors.black, bg = colors.red } },
+
+  inactive = {
+    a = { fg = colors.white, bg = colors.black },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.black, bg = colors.black },
+  },
+}
+
+require('lualine').setup {
+  options = {
+    theme = bubbles_theme,
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
+  },
+  sections = {
+    lualine_a = {
+      { 'mode', separator = { left = '' }, right_padding = 2 },
+    },
+    lualine_b = { 'filename', 'branch' },
+    lualine_c = { 'fileformat' },
+    lualine_x = {},
+    lualine_y = { 'filetype', 'progress' },
+    lualine_z = {
+      { 'location', separator = { right = '' }, left_padding = 2 },
+    },
+  },
+  inactive_sections = {
+    lualine_a = { 'filename' },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { 'location' },
+  },
+  tabline = {},
+  extensions = {},
+}
 
 require("catppuccin").setup({
     flavour = "mocha", -- latte, frappe, macchiato, mocha
