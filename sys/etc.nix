76b31bfe4c410e86dc16383c1b8f56043106fac6
Edvin Källström <edvin.kallstrom@protonmail.com>
2023-02-26 16:40:46 +0100

    fix env variable

--- sys/etc.nix
+++ sys/etc.nix
@@ -7,13 +7,13 @@
       EDITOR = "nvim";
     };
     pathsToLink = [ "/libexec" ];
-    #sessionVariables = rec {
+    sessionVariables = rec {
       LIBVA_DRIVER_NAME="nvidia";
       #XDG_SESSION_TYPE="wayland";
       #GBM_BACKEND="nvidia-drm";
       #__GLX_VENDOR_LIBRARY_NAME="nvidia";
       WLR_NO_HARDWARE_CURSORS="1";
-    #};
+    };
   };
 
   # Something
